29cf787c0463067ff0aed6acabb1f9f81c3338c3
Poleric <nixonleongkb@gmail.com>
2024-08-28 13:11:00 +0800

    test epf thingy

--- main.asm
+++ main.asm
@@ -1,22 +1,32 @@
-.model small
+.model big
 
 .386  ; allow eax
 
 .stack 100
 
 .data
-    val dd 123.45
+    val dd 2400.10
     
 .code
 include utils/print.asm
 include utils/io.asm
+include salary/epf.asm
 
 main proc
     mov ax, @data
     mov ds, ax
 
     fld val
-    call print_float
+    call lookup_epf
+
+    mov ax, bx
+    call print_num_unsigned
+
+    mov dl, 10
+    print
+
+    mov ax, cx
+    call print_num_unsigned
 
     exit 0
 main endp
