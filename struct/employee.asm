00ba90baceb08ee8410fc0b237aead66286f92ac
Poleric <nixonleongkb@gmail.com>
2024-08-28 22:49:56 +0800

    format employee.asm

--- struct/employee.asm
+++ struct/employee.asm
@@ -1,12 +1,11 @@
-EMPLOYEE STRUC
+employee struc
 
-name DB 20 DUP(?)
-orp DD ?            ; ordinary rate of pay
-bonus DD ?
-pto DB ?
-pto_used DB ?
-has_epf DB ?
-has_socso DB ?
+    emp_name DB 20 DUP(?)
+    orp DD ?            ; ordinary rate of pay
+    bonus DD ?
+    pto DB ?
+    pto_used DB ?
+    has_epf DB ?
+    has_socso DB ?
 
-EMPLOYEE ENDS
-
+employee ENDS
