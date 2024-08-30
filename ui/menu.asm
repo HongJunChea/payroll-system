a1bde4dc02eca71319e67cae309de5d8b6f68cdd
Mingyverse <teoh.yongming456@gmail.com>
2024-08-30 17:37:38 +0800

    menu done

--- ui/menu.asm
+++ ui/menu.asm
@@ -1,14 +1,16 @@
 .MODEL SMALL
 .STACK 100
 .DATA
-    WELCOME DB "Diu, what 7 u want. U come here for what???", 10, "$"
-    LINE    DB "==============================================", 10, "$"
-    SEL1    DB "1 - Store Employee Information la", 10, "$"
-    SEL2    DB "2 - View all ur cb frens details", 10, "$"
-    SEL3    DB "3 - Set ur mom monthly performance", 10, "$"
-    SEL4    DB "4 - Show all ur slave's monthly paycheck", 10, "$"
-    PROMPT  DB "Pick one or u suck: $"
-    WRONG   DB "OI PICK 1-4 U GO PICK WHAT?? MEMANG SOHAI PUNYA ORANG", 10, "$"
+    WELCOME DB "Welcome to MKH Payroll System", 10, "$"
+    LINE    DB "================================", 10, "$"
+    SEL1    DB "1 - Store employee information", 10, "$"
+    SEL2    DB "2 - View all employees details", 10, "$"
+    SEL3    DB "3 - Set employee monthly performance", 10, "$"
+    SEL4    DB "4 - View all employees monthly paycheck", 10, "$"
+    SEL5    DB "5 - Exit program", 10, "$"
+    PROMPT  DB "Please select (1 - 4): $"
+    WRONG   DB "Incorrect choice, please try again.", 10, "$"
+    LEAVE   DB "Exiting program....", 10, "$"
     SEL     DB ?
 
 .CODE
@@ -16,20 +18,44 @@
 MAIN PROC
     MOV AX,@DATA
     MOV DS,AX
+    
+    MENU:
+        PUTS WELCOME
+        PUTS LINE
+        PUTS SEL1
+        PUTS SEL2
+        PUTS SEL3
+        PUTS SEL4
+        PUTS PROMPT
+        SCANC SEL
+        CMP SEL,"1"
+        JE EMP_INFO
+        CMP SEL,"2"
+        JE EMP_DET
+        CMP SEL,"3"
+        JE EMP_PERF
+        CMP SEL,"4"
+        JE EMP_PC
+        CMP SEL,"5"
+        JE EXIT
+        PUTS WRONG
+        JMP MENU
+    
+    EMP_INFO:
 
-    PUTS WELCOME
-    PUTS LINE
-    PUTS SEL1
-    PUTS SEL2
-    PUTS SEL3
-    PUTS SEL4
-    PUTS PROMPT
-    PUTS WRONG
+    
+    EMP_DET:
+
+    
+    EMP_PERF:
 
 
+    EMP_PC:
 
 
     EXIT:
+        PUTC 10
+        PUTS LEAVE
         MOV AX,4C00H
         INT 21H
 MAIN ENDP
