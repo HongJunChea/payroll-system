32423bb8157d69faac47568cee585d883b51fac8
Poleric <nixonleongkb@gmail.com>
2024-08-30 12:23:57 +0800

    epf actually works now

--- salary/epf.asm
+++ salary/epf.asm
@@ -1,29 +1,19 @@
 ; Source: https://www.kwsp.gov.my/en/employer/responsibilities/mandatory-contribution
 ;         https://www.kwsp.gov.my/documents/d/guest/jadual-ketiga-bi-pdf-1
 
-include ..\data\epf.asm
-
-; local .old_round, .current_round, .round_bankers, .round_up, .round_down, .round_to_zero
-.old_round     DW ?
-.current_round DW ?
-
-.round_bankers DB 00000000b
-.round_up      DB 00001000b
-.round_down    DB 00000100b
-.round_to_zero DB 00001100b
 ; http://www.website.masmforum.com/tutorials/fptute/fpuchap1.htm
 change_rounding macro round_mode
 
     push ax
 
-    fstcw .current_round          
-    mov ax, .current_round  ; move to ax for controlling ah
-    mov .old_round, ax      ; store for restoring
+    fstcw .current_round
+    mov   ax,         .current_round ; move to ax for controlling ah
+    mov   .old_round, ax             ; store for restoring
 
-    and ah, 11110011b       ; clear round control bit
-    or  ah, round_mode      ; set round control bit
+    and ah, 11110011b  ; clear round control bit
+    or  ah, round_mode ; set round control bit
 
-    mov .current_round, ax  ; move back
+    mov   .current_round, ax ; move back
     fldcw .current_round
 
     pop ax
@@ -31,7 +21,7 @@ change_rounding macro round_mode
 endm
 
 
-restore_rounding macro 
+restore_rounding macro
 
     fldcw .old_round
 
@@ -45,21 +35,16 @@ endm
 ; Returns
 ;   bx: employer payable
 ;   cx: employee payable
-.tmp_word dw ?
-.five_thousand   dw 5000
-.twenty_thousand dw 20000
-
-.twenty          dw 20
-.hundred         dw 100
-
-.four            dw 4
-
-.twelve_percent  dd 0.12
-.eleven_percent  dd 0.11
 lookup_epf proc far
 
-    ficom .twenty_thousand   ; if <= 20000
-    jbe .below20k
+    ficom .twenty_thousand ; if <= 20000
+
+    ; https://stackoverflow.com/questions/33755275/compare-instruction-not-working-correctly
+    fstsw ax  ; load fpu comp
+    fwait
+    sahf
+
+    jbe   .below20k
 
     jmp .above20k
 
@@ -74,19 +59,21 @@ lookup_epf proc far
 
 lookup_above20k proc
 
+    print_char "a"
+
     change_rounding .round_up
 
-    fld st(0)  ; duplicate current salary
-    fmul .twelve_percent
+    fld   st(0)           ; duplicate current salary
+    fmul  .twelve_percent
     frndint
     fistp .tmp_word
-    mov bx, .tmp_word
+    mov   bx, .tmp_word
     
-    fld st(0)  ; duplicate current salary
-    fmul .eleven_percent
+    fld   st(0)           ; duplicate current salary
+    fmul  .eleven_percent
     frndint
     fistp .tmp_word
-    mov cx, .tmp_word
+    mov   cx, .tmp_word
 
     restore_rounding
 
@@ -101,30 +88,39 @@ lookup_below20k proc
     push dx
     push di
 
-    ; formula goes: floor(n / 20) * 20 = index 
+    xor dx, dx
+
     fist .tmp_word
-    mov ax, .tmp_word
 
     ficom .five_thousand
-    jge .20k
+
+    fstsw ax ; load fpu comp
+    fwait
+    sahf
+
+    ja    .20k
 
 .5k:
+    
+    ; formula goes: floor(n / 20) * 20 = index 
+    mov  ax, .tmp_word
 
-    div .twenty  ; get floor of n / 20, wont use remainder
-    mul .four  ; each epf data section is 4 byte wide
-    mov ax, dx
+    div .twenty ; get floor of n / 20, wont use remainder
+    mul .four   ; each epf data section is 4 byte wide
     
     mov bx, offset b5k_contrib
 
     jmp .calculate
 
 .20k:
+
+    ; formula goes: floor(n / 100) * 100 = index 
+    mov  ax, .tmp_word
+
     sub ax, .five_thousand
    
-
-    div .hundred  ; get floor of n / 100, wont use remainder
-    mul .four  ; each epf data section is 4 byte wide
-    mov ax, dx
+    div .hundred ; get floor of n / 100, wont use remainder
+    mul .four    ; each epf data section is 4 byte wide
 
     mov bx, offset b20k_contrib
 
@@ -144,4 +140,4 @@ lookup_below20k proc
 lookup_below20k endp
 
 
-lookup_epf endp
+lookup_epf      endp
