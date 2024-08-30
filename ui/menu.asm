449c04e5efad3f08943c51732191632f0ad40639
Mingyverse <teoh.yongming456@gmail.com>
2024-08-30 17:43:56 +0800

    FAK I FORGOR AGAIN, now fixed ;)

--- ui/menu.asm
+++ ui/menu.asm
@@ -8,7 +8,7 @@
     SEL3    DB "3 - Set employee monthly performance", 10, "$"
     SEL4    DB "4 - View all employees monthly paycheck", 10, "$"
     SEL5    DB "5 - Exit program", 10, "$"
-    PROMPT  DB "Please select (1 - 4): $"
+    PROMPT  DB "Please select (1 - 5): $"
     WRONG   DB "Incorrect choice, please try again.", 10, "$"
     LEAVE   DB "Exiting program....", 10, "$"
     SEL     DB ?
