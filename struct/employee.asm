c4b3085d4d56bd2bf5c30e9a1cd22737bda450bb
Poleric <nixonleongkb@gmail.com>
2024-08-30 16:04:11 +0800

    make emp_name string buff + has_eis field

--- struct/employee.asm
+++ struct/employee.asm
@@ -1,11 +1,14 @@
 employee struc
 
-    emp_name DB 20 DUP(?)
-    orp DD ?            ; ordinary rate of pay
+    emp_name DB 20  ; string length
+             DB ?   ; actual string length
+             DB 20 DUP ("$") 
+    orp DD ?        ; ordinary rate of pay
     bonus DD ?
     pto DB ?
     pto_used DB ?
     has_epf DB ?
     has_socso DB ?
+    has_eis   DB ?
 
 employee ENDS
