e3e61326f60b98b15cd7583b3bd27a9d31d379e3
Poleric <nixonleongkb@gmail.com>
2024-08-31 21:46:05 +0800

    add fields to employee struct and global id tracker

--- structs/emp.asm
+++ structs/emp.asm
@@ -1,14 +1,22 @@
+emp_id_sequence DB 1
+
 employee struc
 
+    emp_id DB "E0000"
     emp_name DB 20  ; string length
              DB ?   ; actual string length
              DB 20 DUP ("$") 
     orp DD ?        ; ordinary rate of pay
     bonus DD ?
     pto DB ?
-    pto_used DB ?
     has_epf DB ?
     has_socso DB ?
     has_eis   DB ?
 
+    ; monthly reports
+    hours_worked DW ?
+    monthly_leaves DB ?
+    overtime_hours DB ?
+    holiday_hours DB ?
+    
 employee ENDS
