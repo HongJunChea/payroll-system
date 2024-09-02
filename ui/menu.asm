2a258a289cec80631f2e9ddb69ef8f44513273d3
Poleric <nixonleongkb@gmail.com>
2024-09-02 09:56:23 +0800

    add new option + link to list emp

--- ui/menu.asm
+++ ui/menu.asm
@@ -1,58 +1,75 @@
 MAIN_MENU PROC
 
     MENU:
-        PUTC 10
-        PUTC 10
-
-        PUTS WELCOME
-        PUTS LINE
-        PUTS SEL1
-        PUTS SEL2
-        PUTS SEL3
-        PUTS SEL4
-        PUTS SEL5
-        PUTS PROMPT
+        call print_menu_options
 
         SCANC SEL
         PUTC 10
         PUTC 10
 
         CMP SEL,"1"
-        JE EMP_INFO
+        JE ADD_EMP
         
         CMP SEL,"2"
-        JE EMP_DET
+        JE LIST_EMP
         
         CMP SEL,"3"
-        JE EMP_PERF
+        JE ADD_EMP_PERFORMANCE
         
         CMP SEL,"4"
-        JE EMP_PC
+        JE LIST_EMP_PAYCHECK
         
         CMP SEL,"5"
+        JE VIEW_EMP_DETAIL
+
+        CMP SEL,"6"
         JE MAIN_MENU_EXIT
         
         PUTS WRONG
         JMP MENU
     
-    EMP_INFO:
+    ADD_EMP:
         CALL create_emp
         JMP MENU
     
-    EMP_DET:
-
+    LIST_EMP:
+        CALL list_all_employee
         JMP MENU
-
     
-    EMP_PERF:
+    ADD_EMP_PERFORMANCE:
 
         JMP MENU
 
-    EMP_PC:
+    LIST_EMP_PAYCHECK:
+
+        JMP MENU
+
+    VIEW_EMP_DETAIL:
 
         JMP MENU
 
     MAIN_MENU_EXIT:
         PUTS EXIT_MSG
-        RET
+    
+    RET
+
 MAIN_MENU ENDP
+
+print_menu_options PROC
+
+    PUTC 10
+    PUTC 10
+
+    PUTS WELCOME
+    PUTS LINE
+    PUTS SEL1
+    PUTS SEL2
+    PUTS SEL3
+    PUTS SEL4
+    PUTS SEL5
+    PUTS SEL6
+    PUTS PROMPT
+
+    ret
+
+print_menu_options ENDP
