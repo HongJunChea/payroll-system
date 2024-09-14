dcae2b8118f5a4044a1b43b95aa6ab9416233958
Poleric <nixonleongkb@gmail.com>
2024-09-14 20:04:55 +0800

    refactor login

--- data/menu/login.asm
+++ data/menu/login.asm
@@ -1,9 +1,7 @@
-.user_id_input db 10 dup(?)
-
-.correct_pass_len db 7
-.correct_pass     db "1234567"
+.input_pass       db 20 dup(?)
+.correct_pass     db "1234567$"
 
 LOGIN_MSG    db 'Welcome to the payroll system!!!!!',10,'$'
 PROMPT_PASS  db 'Please enter your ID: $'
 INVALID_PASS db 'Invalid ID, please try again!!!',10,'$'
-INVALID_PASS db 'Valid ID, you may proceed to MENU!!!',10,'$'
+VALID_PASS   db 'Valid ID, you may proceed to MENU!!!',10,'$'
--- src/menu/login.asm
+++ src/menu/login.asm
@@ -1,38 +1,81 @@
-
+; Prompt the user to login
+; Params:
+;
+; Returns:
+;   zf: 1 - login
+;       0 - failed
 login_menu proc
-;------------------------------------------LOGIN MODULE-----------------------------------------------------
 	puts LOGIN_MSG
-	puts LOGIN2
 
-	ask_id:
-		xor ax, ax ; IF JUMP BACK AGAIN,CLEAR THE PREVIOUS AX DATA
-		puts PROMPT_PASS
+ask_id:    ; loop when fail login
+    puts PROMPT_PASS
 
-;--------------------------------------LET USER ENTER ID----------------------------------------------------
-	xor cx, cx
-	mov cl, .user_id_length
-	lea di, .user_id_input
+    mov di, .input_pass
+    mov ch, length .input_pass
+    call input_password
 
-	ACCEPT_ID:
-		input_char_no_echo
-		stosb
-
-		putc "*"
-
-		loop ACCEPT_ID
-
-;----------------------------------CHECK AND COMPARE ID ENTERED-------------------------------------------------------
-
-	strcmp .user_id_input .correct_id .user_id_length
+	strcmp .input_pass .correct_pass
 	putc 10
-	je VAL_MSG
+	je login_success
 
-	INV_MSG:                        ;DISPLAY MESSAGE IF ID INVALID
-		puts INVALID_PASS
-		jmp ASK_ID
+    ; invalid password
+    puts INVALID_PASS
+    jmp ask_id
 
-	VAL_MSG:                        ;DISPLAY MESSAGE IF ID VALID
-		puts INVALID_PASS
-		ret
+login_success:
+    puts VALID_PASS
+    ret
 
 login_menu endp
+
+
+; gather string inputs while not echoing text typed. Does not include "\n" and "\r"
+; Params:
+;   di: string buffer
+;   ch: max number of chars, including "$"
+; Returns:
+;   cl: number of characters
+input_password proc
+
+    push ax
+    xor cl, cl  ; set cl = 0
+    dec ch      ; to offset the "$" length
+
+input_loop:
+    input_char_no_echo
+
+    cmp al, 8    ; \b
+    je backspace
+
+    cmp al, 13   ; \r
+    je finish
+
+    cmp cl, ch   ; if input_length >= max_length - 1
+    jae input_loop
+
+    ; check within ascii of " " and "~"
+    cmp al, " "
+    jb input_loop
+    cmp al, "~"
+    ja input_loop
+
+    stosb
+    inc cl
+    putc "*"
+
+backspace:
+    putc 8
+    dec cl
+    dec di
+    jmp input_loop
+
+finish:
+    mov [di], "$"
+
+    inc ch     ; restore ch
+    pop ax
+    ret
+
+input_password endp
+
+
