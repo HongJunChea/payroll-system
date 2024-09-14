01adbd807c303dbc03e6e17fe3e7765e2e6f04f7
Poleric <nixonleongkb@gmail.com>
2024-09-15 00:45:49 +0800

    added strtol and strtof

--- src/utils/string.asm
+++ src/utils/string.asm
@@ -40,6 +40,105 @@ terminator:    ; check if str1[i] = str2[i] if either is "$".
 compare_string endp
 
 
+; Convert string into
+; Params
+;   si: pointer to string
+; Returns
+;   ax: the number
+;   dl: error code (0 - ok, 1 - invalid characters)
+strtol proc
+
+    push ax
+    xor ax, ax     ; set ax = 0
+    xor dl, dl     ; set dl = 0
+
+read_loop:
+    mov dl, [si]   ; move each char to dl
+    inc si
+
+    cmp dl, "$"    ; terminate
+    je finish
+
+    call is_digit  ; make sure is digits
+    jne error
+
+    mul ten_b      ; shift previous to the left by 10
+
+    sub dl, "0"    ; convert ascii digit to number
+    add al, dl     ; add onto ax
+
+    jmp read_loop
+
+finish:
+    pop ax
+    mov dl, 0  ; ok
+    ret
+
+error:
+    pop ax
+    mov dl, 1  ; error
+    ret
+
+strtol endp
+
+
+; Convert string into float
+; Params
+;   si: pointer to string
+; Returns
+;   st(0): float
+;   dl: error code (0 - ok, 1 - invalid characters)
+strtof proc
+
+    call strtol
+
+    mov .tmp_word, ax
+    fild .tmp_word
+
+    jne process_fractional  ; if strtol error'd -> there's a character, can be "."
+
+    ; no fractional, ex 9000
+    ret
+
+process_fractional:
+    cmp [si], "."       ; check if the stopped point in on a decimal point
+    jne error           ; if not, means its something else than "."
+
+    inc si
+    call strtol         ; read fractional part as integer
+    jne error
+
+    push cx
+
+    call count_digits
+
+    mov .tmp_word, ax   ; load fractional part as integer
+    fild .tmp_word
+
+    xor ch, ch
+    mov cl, dl          ; divide by 10 ^ number of digits
+    div_10:
+        fidiv TEN_W
+        loop div_10
+
+    faddp st(1), st(0)  ; add integral and fractional part
+
+    pop cx
+
+    mov dl, 0
+    ret
+
+error:
+    fpop
+    mov dl, 1
+    ret
+
+strtof endp
+
+;
+; Macros
+;
+
 strcmp macro str1, str2
 
     push cx
