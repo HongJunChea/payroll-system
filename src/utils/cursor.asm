6426528c4f354f017f6fdafa6b8799610d6aa253
Poleric <nixonleongkb@gmail.com>
2024-09-14 20:03:09 +0800

    put actual functioning cursor utils

--- src/utils/cursor.asm
+++ src/utils/cursor.asm
@@ -1,85 +1,80 @@
-CURSOROFF       MACRO
-        PUSH    AX
-        PUSH    CX
+; Get cursor current position
+; Params
+;   bh: page number
+; Returns
+;   ch: start scan line
+;   cl: end scan line
+;   dh: cursor row
+;   dl: cursor col
+get_cursor_pos proc
 
-        MOV     AH, 1
-        MOV     CH, 28h
-        MOV     CL, 09h
-        INT     10h
+    push ax
 
-        POP     CX
-        POP     AX
-ENDM
+    xor al, al
+    mov ah, 03h
+    int 10h
 
+    pop ax
 
-CURSORON        MACRO
-        PUSH    AX
-        PUSH    CX
-
-        MOV     AH, 1
-        MOV     CH, 08h
-        MOV     CL, 09h
-        INT     10h
-
-        POP     CX
-        POP     AX
-ENDM
+get_cursor_pos endp
 
 
-GOTOXY  MACRO   col, row
-        PUSH    AX
-        PUSH    BX
-        PUSH    DX
+; Set cursor position
+; Params:
+;   bh: page number
+;   dh: cursor row
+;   dl: cursor col
+set_cursor_pos proc
 
-        MOV     AH, 02h
-        MOV     DH, row
-        MOV     DL, col
-        MOV     BH, 0
-        INT     10h
-        
-        POP     DX
-        POP     BX
-        POP     AX
-ENDM
+    push ax
 
+    xor al, al
+    mov ah, 02h
+    int 10h
 
-CLEAR_SCREEN PROC FAR
-    PUSH AX ; store registers...
-    PUSH DS ;
-    PUSH BX ;
-    PUSH CX ;
-    PUSH DI ;
+    pop ax
 
-    MOV AX, 40h
-    MOV DS, AX   ; for getting screen parameters.
-    MOV AH, 06h  ; scroll up function id.
-    MOV AL, 0    ; scroll all lines!
-    MOV BH, 07   ; attribute for new lines.
-    MOV CH, 0    ; upper row.
-    MOV CL, 0    ; upper col.
-    MOV DI, 84h  ; rows on screen -1,
-    MOV DH, [DI] ; lower row (byte).
-    MOV DI, 4Ah  ; columns on screen,
-    MOV DL, [DI]
-    DEC DL       ; lower col.
-    INT 10h
-
-    ; set cursor position to top
-    ; of the screen:
-    MOV BH, 0  ; current page.
-    MOV DL, 0  ; col.
-    MOV DH, 0  ; row.
-    MOV AH, 02
-    INT 10h
-
-    POP DI ; re-store registers...
-    POP CX ;
-    POP BX ;
-    POP DS ;
-    POP AX ;
-
-    RET
-CLEAR_SCREEN ENDP
+set_cursor_proc endp
 
 
-END
+; Scroll down the screen given the number of lines
+; Params:
+;   al: lines to scroll, 0 = clear
+;   bh: background color and foreground color. https://en.wikipedia.org/wiki/BIOS_color_attributes
+;
+;   if al = 0:
+;   ch: upper row number
+;   cl: left column number
+;   dh: lower row number
+;   dl: right column number
+scroll_down proc
+
+    push ax
+
+    mov ah, 06h
+    int 10h
+
+    pop ax
+
+scroll_down endp
+
+
+; Clears the current screen
+; Params:
+;
+; Returns:
+;
+clear_screen proc
+
+    push ax
+    push cx
+    push dx
+
+    mov al, 0
+    call scroll_down
+
+    pop dx
+    pop cx
+    pop ax
+
+clear_screen endp
