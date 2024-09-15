99a02c9a710741ded4b29f241b261b710564adbb
Poleric <nixonleongkb@gmail.com>
2024-09-15 17:52:50 +0800

    struct for storing temporary payroll value

--- data/menu/pay/tmp_pay.asm
+++ data/menu/pay/tmp_pay.asm
@@ -1,9 +1,14 @@
-.emp_perf_basic_tmp      dd 0
-.emp_perf_basic_tmp      dd 0
-.emp_perf_ph_tmp         dd 0
-.emp_perf_earn_total_tmp dd 0
+.payroll struc
+    ; earnings
+    basic      dd 0
+    ot         dd 0
+    ph         dd 0
+    earn_total dd 0
 
-.emp_perf_epf_tmp        dd 0
-.emp_perf_socso_tmp      dd 0
-.emp_perf_eis_tmp        dd 0
-.emp_perf_ded_total_tmp  dd 0
+    ; deductions
+    epf          dd 0
+    socso        dd 0
+    eis          dd 0
+    deduct_total dd 0
+
+payroll ends
