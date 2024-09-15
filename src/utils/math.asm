9e34c53a9f82984875ae4753160994fbe581ce45
Poleric <nixonleongkb@gmail.com>
2024-09-16 02:14:23 +0800

    use constant 1/log 2 10

--- src/utils/math.asm
+++ src/utils/math.asm
@@ -25,23 +25,17 @@ min endp
 count_digits proc
 
     ; counts digit using change of base: log a (b) = log c (b) / log c (a)
-    ; formula is: floor(log 10 (n) + 1)
-
-    ; could be approximated and stored in a variable but eh
-    fld1                 ; load 1.0
-    fldl2t               ; load log 2 (10)
-    fdivp st(1), st(0)   ; 1 / log 2 (10)
+    ; formula is: floor(log 10 (n)) + 1
+    fld INVLOG2T
 
     mov .tmp_word, ax
     fild .tmp_word
 
     fyl2x               ; st(1) * log 2 (st(0))
 
-    fld1                ; log 10 (n) + 1
-    faddp st(1), st(0)
-
     fistp .tmp_word     ; floor and store result
     mov dl, byte ptr [.tmp_word]
+    inc dl              ; + 1
 
     ret
 
