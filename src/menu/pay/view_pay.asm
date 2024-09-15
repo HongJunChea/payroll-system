b26d2771eb26d458830f922a3fb083df9d0a77f1
Poleric <nixonleongkb@gmail.com>
2024-09-15 18:55:09 +0800

    refactor view pay

--- src/menu/pay/view_pay.asm
+++ src/menu/pay/view_pay.asm
@@ -1,209 +1,272 @@
+; View employee full payroll summary in pages.
+; Params
+;   bx: pointer to employee
 view_pay proc
 
-    puts EMP_PERF_HEADER
+    ; calculate before printing
+    call calculate_total
+
+    puts EMP_PAY_HEADER
     puts BOLD_BORDER
-
-    call print_perf_dets
-
+    call print_pay_employee_details
     puts THIN_BORDER
 
-    puts LS_EMPS_PERF_HEADER
-    call print_emp_perf_row
+    call press_any_key_to_continue
 
     puts THIN_BORDER
-
-    putc 10
-    puts PRESS_ANY_KEY
-    input_char_no_echo
-    putc 10
-
-    call print_perf_earnings
-
+    call print_pay_details
     puts THIN_BORDER
 
-    call print_perf_deducts
-
-    puts THIN_BORDER
-
-    call print_perf_total
-
+    puts EMP_PAY_TOTAL
+    putf payroll.total
     putc 10
 
-    puts PRESS_ANY_KEY
-    input_char_no_echo
-
-    putc 10
-
+    call press_any_key_to_continue
     ret
 
 view_pay endp
 
 
-print_perf_dets proc
+; Print summary of employee details
+; Params
+;   bx: pointer to employee
+print_pay_employee_details proc
 
-    puts EMP_PERF_NAME
-    putsn_b [BX].emp_name [BX].emp_name_length
+    ; Row 1
+    puts EMP_PAY_NAME
+    puts [bx].emp_name
 
     putc_n " " 3
 
-    puts EMP_PERF_ID
-    putsn [bx].emp_id EMP_ID_LEN
+    puts EMP_PAY_ID
+    puts [bx].emp_id
 
     putc 10
 
-    puts EMP_PERF_JOB
+    ; Row 2
+    puts EMP_PAY_JOB
     call print_job_type
 
     putc 10
 
+    ; Row 3
+    puts EMP_PAY_SOCSO
     mov al, [bx].has_socso
-    puts EMP_PERF_SOCSO
     call print_bool
 
     putc_n " " 3
 
+    puts EMP_PAY_EPF
     mov al, [bx].has_epf
-    puts EMP_PERF_EPF
     call print_bool
 
     putc_n " " 3
 
+    puts EMP_PAY_EIS
     mov al, [bx].has_eis
-    puts EMP_PERF_EIS
     call print_bool
 
     putc 10
     ret
 
-print_perf_dets endp
+print_pay_employee_details endp
 
 
-print_perf_earnings proc
+move_right macro
 
-    puts EMP_PERF_EARN_HEADER
+    add dl, 18
+    call set_cursor_pos
 
-    call calculate_basic
-    fst .emp_perf_basic_tmp
+    putc "|"
 
-        puts EMP_PERF_BASIC
-        call print_float
+    add dl, 1
+    call set_cursor_pos
 
-        putc 10
-    ; skip_calc_basic:
-    call calculate_ot
-    fst .emp_perf_basic_tmp
+endm
 
-    cmp [bx].overtime_hours, 0
-    jbe skip_calc_ot
+; Print employee payroll details in tabular form
+; Params
+;   bx: pointer to employee
+print_pay_details proc
 
-        puts EMP_PERF_OT
-        call print_float
+    ; Header
+    call get_cursor_pos
+    puts EMP_PAY_EARN_HEADER
 
-        putc 10
-    skip_calc_ot:
-    call calculate_ph
-    fst .emp_perf_ph_tmp
+    move_right
 
-    cmp [bx].holiday_hours, 0
-    jbe skip_calc_ph
+    call EMP_PAY_DEDUC_HEADER
 
-        puts EMP_PERF_PH
-        call print_float
+    ; Row 1
+    call get_cursor_pos
+    call print_pay_basic
 
-        putc 10
-    skip_calc_ph:
-    mov cx, 3
-    call calculate_subtotal
+    move_right
 
-    puts EMP_PERF_EARN_TOTAL
-    call print_float
-
-    fstp .emp_perf_earn_total_tmp
-
+    call print_pay_socso
     putc 10
+
+    ; Row 2
+    call get_cursor_pos
+    call print_pay_ot
+
+    move_right
+
+    call print_pay_epf
+    putc 10
+
+    ; Row 3
+    call get_cursor_pos
+    call print_pay_ph
+
+    move_right
+
+    call print_pay_eis
+    putc 10
+
+    ; Row 4
+    call get_cursor_pos
+    call print_pay_earn_subtotal
+
+    move_right
+
+    call print_pay_deduc_subtotal
+    putc 10
+
     ret
 
-print_perf_earnings endp
+print_pay_details endp
 
 
-print_perf_deducts proc
+; Print employee basic salary.
+; Position to print is row 1, left
+; Params
+;   payroll.basic
+print_pay_basic proc
 
-    puts EMP_PERF_DEDUC_HEADER
-
-    call calculate_socso
-    fst .emp_perf_socso_tmp
-
-    cmp [bx].has_socso, 0
-    je skip_calc_socso
-
-        puts EMP_PERF_DEDUC_SOCSO
-        call print_float
-
-        putc 10
-    skip_calc_socso:
-    call calculate_epf
-    fst .emp_perf_epf_tmp
-
-    cmp [bx].has_epf, 0
-    je skip_calc_epf
-
-        puts EMP_PERF_DEDUC_EPF
-        call print_float
-
-        putc 10
-    skip_calc_epf:
-    call calculate_eis
-    fst .emp_perf_eis_tmp
-
-    cmp [bx].has_eis, 0
-    je skip_calc_eis
-
-        puts EMP_PERF_DEDUC_EIS
-        call print_float
-
-        putc 10
-    skip_calc_eis:
-    mov cx, 3
-    call calculate_subtotal
-
-    puts EMP_PERF_DEDUC_TOTAL
-    call print_float
-
-    fstp .emp_perf_ded_total_tmp
-
-    putc 10
+    putf payroll.basic
     ret
 
-print_perf_deducts endp
+print_pay_basic endp
 
 
-print_perf_total proc
+; Print employee overtime rate. Wont print if no overtime worked.
+; Position to print is row 2, left
+; Params
+;   bx: pointer to employee
+;   payroll.ot
+print_pay_ot proc
 
-    puts EMP_PERF_EARN_TOTAL
-    fld .emp_perf_earn_total_tmp
-    call print_float
+    cmp [bx].overtime_hours, 0  ; if no overtime hours
+    jbe skip
 
-    putc 10
+    putf payroll.ot
 
-    puts EMP_PERF_DEDUC_TOTAL
-    fld .emp_perf_ded_total_tmp
-    call print_float
-
-    putc 10
-    putc 10
-
-    puts EMP_PERF_NET_TOTAL
-    fsubp st(1), st(0)
-    call print_float
-
-    putc 10
+skip:
     ret
 
-print_perf_total endp
+print_pay_ot endp
 
 
+; Print employee public holiday rate. Wont print if no public holiday worked.
+; Position to print is row 3, left
+; Params
+;   bx: pointer to employee
+;   payroll.ph
+print_pay_ph proc
+
+    cmp [bx].holiday_hours, 0  ; if no public holiday hours
+    jbe skip
+
+    putf payroll.ph
+
+skip:
+    ret
+
+print_pay_ph endp
 
 
+; Print employee earning subtotal.
+; Position to print is row 4, left
+; Params
+;   payroll.earn_total
+print_pay_earn_subtotal proc
+
+    putf payroll.earn_total
+    ret
+
+print_pay_earn_subtotal endp
+
+
+; Print employee socso deductions. Wont print if no socso.
+; Position to print is row 1, right
+; Params
+;   bx: pointer to employee
+;   payroll.socso
+print_pay_socso proc
+
+    cmp [bx].has_socso, 0  ; if no socso
+    je skip
+
+    putf payroll.socso
+
+skip:
+    ret
+
+print_pay_socso endp
+
+
+; Print employee epf deductions. Wont print if no epf.
+; Position to print is row 2, right
+; Params
+;   bx: pointer to employee
+;   payroll.epf
+print_pay_epf proc
+
+    cmp [bx].has_epf, 0  ; if no epf
+    je skip
+
+    putf payroll.epf
+
+skip:
+    ret
+
+print_pay_epf endp
+
+
+; Print employee eis deductions. Wont print if no eis.
+; Position to print is row 3, right
+; Params
+;   bx: pointer to employee
+;   payroll.eis
+print_pay_eis proc
+
+    cmp [bx].has_eis, 0  ; if no eis
+    je skip
+
+    putf payroll.eis
+
+skip:
+    ret
+
+print_pay_eis endp
+
+
+; Print employee deductions subtotal.
+; Position to print is row 4, right
+; Params
+;   payroll.deduc_total
+print_pay_deduc_subtotal proc
+
+    putf payroll.deduc_total
+    ret
+
+print_pay_deduc_subtotal endp
+
+
+; Utility to print the text "Fulltime" or "Parttime" based on employee job type
+; Params
+;   bx: pointer to employee
 print_job_type proc
 
     cmp [bx].job_type, 1
@@ -214,12 +277,12 @@ print_job_type proc
     puts EMP_JOB_TYPE_INVALID_MSG
     ret
 
-perf_job_pt:
-    puts EMP_PERF_PT
+is_pt:
+    puts EMP_PT_MSG
     ret
 
-perf_job_ft:
-    puts EMP_PERF_FT
+is_ft:
+    puts EMP_FT_MSG
     ret
 
 print_job_type endp
