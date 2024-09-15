b054965fdf67cbdfa295be611d356a0f61da73be
Poleric <nixonleongkb@gmail.com>
2024-09-15 18:55:02 +0800

    add fields to payroll struct

--- data/menu/pay/tmp_pay.asm
+++ data/menu/pay/tmp_pay.asm
@@ -1,4 +1,4 @@
-.payroll struc
+payroll struc
     ; earnings
     basic      dd 0
     ot         dd 0
@@ -6,9 +6,11 @@
     earn_total dd 0
 
     ; deductions
-    epf          dd 0
-    socso        dd 0
-    eis          dd 0
-    deduct_total dd 0
+    epf         dd 0
+    socso       dd 0
+    eis         dd 0
+    deduc_total dd 0
+
+    total       dd 0
 
 payroll ends
