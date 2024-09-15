99749859bebef5108df4c34d376d6c6d39b30cfe
Poleric <nixonleongkb@gmail.com>
2024-09-15 15:29:25 +0800

    make emp name no buffer

--- data/emp.asm
+++ data/emp.asm
@@ -5,22 +5,20 @@ HOLIDAY_RATE DD 3.0
 
 employee struc
 
-    emp_id DB "E0000$"
-    emp_name_buffer DB 20  ; string length
-    emp_name_length DB ?   ; actual string length
-    emp_name        DB 20 DUP ("$") 
-    job_type        DB ?   ; 1 = parttime, 2 = fulltime
-    orp DD ?        ; ordinary rate of pay
-    pto DW ?
-    has_epf DB ?
-    has_socso DB ?
-    has_eis   DB ?
+    emp_id     DB "E0000$"
+    emp_name   DB 19 DUP (?), "$"
+    job_type   DB ?   ; 1 = parttime, 2 = fulltime
+    orp        DD ?   ; ordinary rate of pay
+    pto        DW ?   ; paid time off, in hours
+    has_epf    DB ?
+    has_socso  DB ?
+    has_eis    DB ?
 
     ; monthly reports
-    filled_performance DB 0    ; boolean to show filled performance
-    hours_worked DW ?
-    monthly_leaves DW ?  ; only used for fulltime
-    overtime_hours DW ?
-    holiday_hours DW ?
+    filled_performance DB 0   ; boolean to show filled performance
+    hours_worked       DW ?
+    monthly_leaves     DW ?   ; only used for fulltime
+    overtime_hours     DW ?
+    holiday_hours      DW ?
     
 employee ENDS
